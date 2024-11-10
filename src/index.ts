a2c92b03cdfa5d21399f6816cb44e8abd94b0e60
Kemal Rovčanin <kemo996@gmail.com>
2024-11-10 10:21:54 +0100

    add server setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
 import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import { aggregateCityData } from './services/aggregator.js';
 
-dotenv.config();
-aggregateCityData('Sarajevo')
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/weather', (req: express.Request, res: express.Response) => {
+  aggregateCityData(req.query.city as string)
+    .then((data) => res.json(data))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while processing your request' });
+    });
+});
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
+});
